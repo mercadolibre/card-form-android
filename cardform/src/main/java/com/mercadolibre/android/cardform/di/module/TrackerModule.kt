3a241babecb7df6238ad3c7f3a1e18dca64dde7f
Jorge <jorge.gonzalez@mercadolibre.com>
2020-01-14 12:17:28 -0300

    review code

--- cardform/src/main/java/com/mercadolibre/android/cardform/di/module/TrackerModule.kt
+++ cardform/src/main/java/com/mercadolibre/android/cardform/di/module/TrackerModule.kt
@@ -5,10 +5,10 @@ import com.mercadolibre.android.cardform.tracks.TrackerData
 import com.mercadopago.android.px.addons.TrackingBehaviour
 
 class TrackerModule(
-    private val siteId: String,
-    private val flowId: String,
-    private val sessionId: String,
-    private val trackingBehaviour: TrackingBehaviour
+    siteId: String,
+    flowId: String,
+    sessionId: String,
+    trackingBehaviour: TrackingBehaviour
 ) {
 
     val tracker by lazy {
