6a563f1874d734cdcffc32ec2878549decfa440d
Guche Mantuano <agustin.mantuano@mercadolibre.com>
2020-01-15 17:44:34 -0300

    Fix edit text input types

--- cardform/src/main/java/com/mercadolibre/android/cardform/presentation/helpers/KeyboardHelper.kt
+++ cardform/src/main/java/com/mercadolibre/android/cardform/presentation/helpers/KeyboardHelper.kt
@@ -1,6 +1,7 @@
 package com.mercadolibre.android.cardform.presentation.helpers
 
 import android.support.v4.app.Fragment
+import android.view.View
 import com.mercadolibre.android.cardform.presentation.extensions.addKeyBoardListener
 import com.mercadolibre.android.cardform.presentation.extensions.hideKeyboard
 import com.mercadolibre.android.cardform.presentation.extensions.showKeyboard
@@ -25,6 +26,12 @@ object KeyboardHelper {
     fun showKeyboard(fragment: Fragment?) {
         if (!keyboardShowing) {
             fragment?.showKeyboard()
+        }
+    }
+
+    fun showKeyboard(view: View) {
+        if (!keyboardShowing) {
+            view.showKeyboard()
         }
     }
 }
