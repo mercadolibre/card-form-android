d32351cbcdf6cb0f0da5ab22c0e5845ec9d9c7b4
Guche Mantuano <agustin.mantuano@mercadolibre.com>
2020-01-16 15:50:31 -0300

    [ci deploy]

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -21,5 +21,5 @@ object Versions {
     const val kotlinCoroutines = "1.3.2"
     const val picassoDiskCache = "1.+"
 
-    const val versionToDeploy = "0.0.3"
+    const val versionToDeploy = "1.0.0"
 }
