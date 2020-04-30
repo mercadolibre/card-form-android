75ce567a350b1c3deefe505a57c16d1fb6fd2e63
Jorge <jorge.gonzalez@mercadolibre.com>
2020-04-30 13:50:11 -0300

    refactor parcelable cardUi

--- cardform/src/main/java/com/mercadolibre/android/cardform/data/model/response/CardUi.kt
+++ cardform/src/main/java/com/mercadolibre/android/cardform/data/model/response/CardUi.kt
@@ -45,41 +45,6 @@ internal data class CardUi (
     }
 
     override fun describeContents() = 0
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as CardUi
-
-        if (cardNumberLength != other.cardNumberLength) return false
-        if (cardColor != other.cardColor) return false
-        if (cardFontColor != other.cardFontColor) return false
-        if (cardFontType != other.cardFontType) return false
-        if (securityCodeLocation != other.securityCodeLocation) return false
-        if (securityCodeLength != other.securityCodeLength) return false
-        if (paymentMethodImageUrl != other.paymentMethodImageUrl) return false
-        if (issuerImageUrl != other.issuerImageUrl) return false
-        if (!cardPattern.contentEquals(other.cardPattern)) return false
-        if (validation != other.validation) return false
-        if (extraValidations != other.extraValidations) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = cardNumberLength
-        result = 31 * result + cardColor.hashCode()
-        result = 31 * result + cardFontColor.hashCode()
-        result = 31 * result + cardFontType.hashCode()
-        result = 31 * result + securityCodeLocation.hashCode()
-        result = 31 * result + securityCodeLength
-        result = 31 * result + (paymentMethodImageUrl?.hashCode() ?: 0)
-        result = 31 * result + (issuerImageUrl?.hashCode() ?: 0)
-        result = 31 * result + cardPattern.contentHashCode()
-        result = 31 * result + validation.hashCode()
-        result = 31 * result + extraValidations.hashCode()
-        return result
-    }
 
     companion object CREATOR : Parcelable.Creator<CardUi> {
         override fun createFromParcel(parcel: Parcel) = CardUi(parcel)
