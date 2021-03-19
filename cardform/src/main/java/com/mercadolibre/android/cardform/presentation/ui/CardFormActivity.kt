8b26b39494952f42d9a8f75495cfbb2e62930523
Alejandro Gaidolfi <alejandro.gaidolfi@mercadolibre.com>
2021-03-19 16:54:28 -0300

    custom style added. status bar view added and managed with insets

--- cardform/src/main/java/com/mercadolibre/android/cardform/presentation/ui/CardFormActivity.kt
+++ cardform/src/main/java/com/mercadolibre/android/cardform/presentation/ui/CardFormActivity.kt
@@ -1,15 +1,21 @@
 package com.mercadolibre.android.cardform.presentation.ui
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat.setOnApplyWindowInsetsListener
 import androidx.fragment.app.Fragment
 import com.mercadolibre.android.cardform.CARD_FORM_EXTRA
 import com.mercadolibre.android.cardform.CardForm
 import com.mercadolibre.android.cardform.EXIT_ANIM_EXTRA
 import com.mercadolibre.android.cardform.R
 import com.mercadolibre.android.cardform.internal.CardFormWithFragment
+import kotlinx.android.synthetic.main.activity_card_form.view.*
 
 internal class CardFormActivity : AppCompatActivity() {
 
@@ -25,6 +31,33 @@ internal class CardFormActivity : AppCompatActivity() {
                 commitAllowingStateLoss()
             }
         }
+        setOnApplyWindowInsetsListener(window.decorView) { v, insets ->
+            v.apply {
+                window.decorView.status_bar_place_holder.apply {
+                    layoutParams.height = insets.stableInsetTop
+                    visibility = View.VISIBLE
+                    setBackgroundColor(
+                        resources.getColor(R.color.ui_meli_red))
+                }
+            }
+            insets
+        }
+        setUpWindow()
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun setUpWindow() {
+        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+
+        val decorView = window.decorView
+        decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR or
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
     }
 
     override fun finish() {
