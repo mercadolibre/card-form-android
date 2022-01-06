c41d659a1c621760eed9707a202ea14f65529a13
Luis Fernando Lopes Silva <luis.fsilva@mercadolibre.com>
2022-01-06 16:02:13 -0300

    create junit package domain

---
+++ cardform/src/test/java/com/mercadolibre/android/cardform/domain/AssociatedCardUseCaseTest.kt
@@ -0,0 +1,41 @@
+package com.mercadolibre.android.cardform.domain
+
+import com.mercadolibre.android.cardform.TestContextProvider
+import com.mercadolibre.android.cardform.data.repository.CardAssociationRepositoryImpl
+import com.mercadolibre.android.cardform.data.service.CardAssociationService
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class AssociatedCardUseCaseTest {
+
+    @Nested
+    @DisplayName("Given away that I add a new card")
+    inner class GivenAwayThatAddNewCard {
+
+        @Nested
+        @DisplayName("When the card associated is activated")
+        inner class WhenTheCardAssociatedIsSuccessful {
+
+            private val associatedCardParam = mockk<AssociatedCardParam>(relaxed = true)
+            private val cardAssociationService = mockk<CardAssociationService>(relaxed = true)
+            private val contextProvider = TestContextProvider()
+            private val cardAssociationRepositoryImpl = CardAssociationRepositoryImpl(cardAssociationService,
+                acceptThirdPartyCard = true,
+                activateCard = true,
+                contextProvider = contextProvider
+            )
+            private val subject = AssociatedCardUseCase(cardAssociationRepositoryImpl)
+
+            @Test
+            fun `Then the associated card is activated`() {
+                runBlocking {
+                    assertNotNull(subject.execute(associatedCardParam))
+                }
+            }
+        }
+    }
+}
