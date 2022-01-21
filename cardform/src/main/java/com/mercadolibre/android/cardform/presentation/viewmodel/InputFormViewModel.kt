d45625bccf1e9013bbaf4af1a4f584e566b7a77e
Diego Cardoso <diego.cardoso@mercadolivre.com>
2022-01-20 23:36:29 -0300

    added new fields in 3 tracks events

--- cardform/src/main/java/com/mercadolibre/android/cardform/presentation/viewmodel/InputFormViewModel.kt
+++ cardform/src/main/java/com/mercadolibre/android/cardform/presentation/viewmodel/InputFormViewModel.kt
@@ -197,7 +197,14 @@ internal class InputFormViewModel(
             cardLiveData.postValue(CardDataMapper.map(this))
             issuersLiveData.postValue(ArrayList(issuers))
             loadInputData(this)
-            tracker.trackEvent(BinRecognizedTrack())
+            tracker.trackEvent(
+                BinRecognizedTrack(
+                    binValidator.getLastValidBin().substring(0..5),
+                    issuer?.id ?: 0,
+                    paymentMethod?.paymentMethodId!!,
+                    paymentMethod?.paymentTypeId!!
+                )
+            )
         }
     }
 
@@ -225,7 +232,11 @@ internal class InputFormViewModel(
             tracker.trackEvent(
                 ErrorTrack(
                     TrackApiSteps.TOKEN.getType(),
-                    throwable.message.orEmpty()
+                    throwable.message.orEmpty(),
+                    binValidator.getLastValidBin(),
+                    issuer?.id ?: 0,
+                    paymentMethod?.paymentMethodId!!,
+                    paymentMethod?.paymentTypeId!!
                 )
             )
             stateUiLiveData.postValue(ErrorUtil.createError(throwable))
@@ -244,7 +255,11 @@ internal class InputFormViewModel(
             tracker.trackEvent(
                 ErrorTrack(
                     TrackApiSteps.ASSOCIATION.getType(),
-                    throwable.message.orEmpty()
+                    throwable.message.orEmpty(),
+                    binValidator.getLastValidBin(),
+                    issuer?.id ?: 0,
+                    paymentMethod?.paymentMethodId!!,
+                    paymentMethod?.paymentTypeId!!
                 )
             )
             stateUiLiveData.postValue(ErrorUtil.createError(throwable))
@@ -337,6 +352,15 @@ internal class InputFormViewModel(
 
     fun trackBack() {
         tracker.trackEvent(BackTrack())
+    }
+
+    fun trackValidBinNumber(){
+        tracker.trackEvent(BinValidTrack(
+            binValidator.getLastValidBin(),
+            issuer?.id?: 0,
+            paymentMethod?.paymentMethodId!!,
+            paymentMethod?.paymentTypeId!!
+        ))
     }
 
     companion object {
