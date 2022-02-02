85a12c9bb7268aa5b1500da6e196345a48bb0e19
Cristian Tovar <cristian.tovar@mercadolibre.com.co>
2022-02-02 13:11:34 -0500

    test BinValidTrackTest and BinRecognizedTrackTest are modified

--- cardform/src/test/java/com/mercadolibre/android/cardform/tracks/model/bin/BinRecognizedTrackTest.kt
+++ cardform/src/test/java/com/mercadolibre/android/cardform/tracks/model/bin/BinRecognizedTrackTest.kt
@@ -6,8 +6,14 @@ internal class BinRecognizedTrackTest {
 
     @Test
     fun validateBinRecognizedTrack() {
-        val binRecognizedTrack = BinRecognizedTrack()
+        val binRecognizedTrack = BinRecognizedTrack("mock_bin", 1, "mock_paymentMethodId", "mock_paymentMethodType")
+        val map = mutableMapOf<String, Any>()
+        val mockMap :MutableMap<String, Any> = mutableMapOf("bin" to  "mock_bin", "issuer" to 1, "payment_method_id" to "mock_paymentMethodId", "payment_method_type_id" to "mock_paymentMethodType")
+
+        binRecognizedTrack.addTrackData(map)
+
         assert(!binRecognizedTrack.trackGA)
         assert("/card_form/bin_number/recognized" == binRecognizedTrack.pathEvent)
+        assert(mockMap.size == map.size)
     }
 }
--- cardform/src/test/java/com/mercadolibre/android/cardform/tracks/model/bin/BinValidTrackTest.kt
+++ cardform/src/test/java/com/mercadolibre/android/cardform/tracks/model/bin/BinValidTrackTest.kt
@@ -6,8 +6,14 @@ internal class BinValidTrackTest {
 
     @Test
     fun validateBinValidTrack() {
-        val binValidTrack = BinValidTrack()
+        val binValidTrack = BinValidTrack("mock_bin", 1, "mock_paymentMethodId", "mock_paymentMethodType")
+        val map = mutableMapOf<String, Any>()
+        val mockMap :MutableMap<String, Any> = mutableMapOf("bin" to  "mock_bin", "issuer" to 1, "payment_method_id" to "mock_paymentMethodId", "payment_method_type_id" to "mock_paymentMethodType")
+
+        binValidTrack.addTrackData(map)
+
         assert(!binValidTrack.trackGA)
         assert("/card_form/bin_number/valid" == binValidTrack.pathEvent)
+        assert(mockMap.size == map.size)
     }
 }
